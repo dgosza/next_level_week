70b0617c5be260614f568bda20d171a3af4321a0
dieezs <diegomagasouza@gmail.com>
2020-06-03 23:38:27 -0300

    still doing

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,5 +1,4 @@
 
-
 function populateUfs() {
     const ufSelect = document.querySelector("select[name=uf]");
 
@@ -26,19 +25,39 @@ function getCities(event) {
     stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `http://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    citieSelect.innerHTML = ''
+    citieSelect.disabled = true
 
     fetch(url)
         .then(res => res.json())
         .then(cities => {
-
             for (const city of cities) {
                 citieSelect.innerHTML += `<option value="${city.id}"> ${city.nome} </option>`
             }
-
             citieSelect.disabled = false
+
 
         })
 }
 
 
 document.querySelector("select[name=uf]").addEventListener("change", getCities)
+
+//Itens de Coleta
+//pegar todo os li
+
+const itensToCollect = document.querySelectorAll(".items-grid li");
+
+for(const item of itensToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+function handleSelectedItem(event) {
+    const itemLi = event.target;
+    //add or remove uma classe com js
+    
+    itemLi.classList.toggle("selected")
+
+    const itemId = event.target.dataset.id;
+
+}
